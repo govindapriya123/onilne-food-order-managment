6e96488152f44226a37a761bcaa1aaa84a0d8cd4
govindapriya123 <83416415+govindapriya123@users.noreply.github.com>
2022-10-29 19:26:18 +0530

    fonts

--- footer.php
+++ footer.php
@@ -1,7 +1,28 @@
-<!--footer section-->
-<div class="footer">
-            <div class="wrapper">
-            <p class="text-center">2022 All rights reserved, some restaurant, developed by <a href="#">Govindapriya</a></p>
-</div>
-</div>
-        <!--footer section ends-->
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By <a href="#">Govindapriya</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
